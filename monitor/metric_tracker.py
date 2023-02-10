051041d6b99cc08db6cfb983c89ca869e8264c8b
huweizheng <huweizheng@sensetime.com>
2023-02-10 15:24:34 +0800

    feat: update codes

--- monitor/metric_tracker.py
+++ monitor/metric_tracker.py
@@ -1,4 +1,5 @@
 import os
+import re
 import socket
 import time
 from threading import Thread
@@ -29,78 +30,78 @@ class MetricTracker(Thread):
             return float(f"{num:.2f}")
 
         while not self.stopped:
-            job_id = "none"
-            if os.getenv("SLURM_JOB_ID") is not None:
-                job_id = os.getenv("SLURM_JOB_ID")
+            try:
+                job_id = "none"
+                if os.getenv("SLURM_JOB_ID") is not None:
+                    job_id = os.getenv("SLURM_JOB_ID")
 
-            job_name = "none"
-            if os.getenv("SLURM_JOB_NAME") is not None:
-                job_name = os.getenv("SLURM_JOB_NAME")
+                job_name = "none"
+                if os.getenv("SLURM_JOB_NAME") is not None:
+                    job_name = os.getenv("SLURM_JOB_NAME")
 
-            key = f"{job_id}_{job_name}"
+                key = f"{job_id}_{job_name}"
 
-            hostname = socket.gethostname()
-            timestamp = int(time.time())
+                hostname = socket.gethostname()
+                timestamp = int(time.time())
 
-            cpu_util = format_2_decimal(psutil.cpu_percent())
+                cpu_util = format_2_decimal(psutil.cpu_percent())
 
-            mem = psutil.virtual_memory()
-            mem_util = format_2_decimal(mem[2])
+                mem = psutil.virtual_memory()
+                mem_util = format_2_decimal(mem[2])
 
-            network_io = psutil.net_io_counters(pernic=True)
-            for interface in list(network_io.keys()):
-                if "lo" in interface or "bond" in interface:
-                    del network_io[interface]
+                raw_network_io = psutil.net_io_counters(pernic=True)
+                ib_network_io = {}
+                for interface in raw_network_io:
+                    if re.search("^ib[0-9]+", interface):
+                        ib_network_io[interface] = raw_network_io[interface]
 
-            gpu_rank = "none"
-            if os.getenv("SLURM_PROCID") is not None:
-                gpu_rank = int(os.getenv("SLURM_PROCID"))
+                gpu_rank = "none"
+                if os.getenv("SLURM_PROCID") is not None:
+                    gpu_rank = int(os.getenv("SLURM_PROCID"))
 
-            local_gpu_rank = "none"
-            if os.getenv("SLURM_LOCALID") is not None:
-                local_gpu_rank = os.getenv("SLURM_LOCALID")
+                local_gpu_rank = "none"
+                if os.getenv("SLURM_LOCALID") is not None:
+                    local_gpu_rank = int(os.getenv("SLURM_LOCALID"))
 
-            metric_dict = {
-                "key": key,
-                "timestamp": timestamp,
-                "hostname": hostname,
-                "cpu_util": cpu_util,
-                "mem_util": mem_util,
-                "network_io": network_io,
-                "gpu_rank": gpu_rank,
-                "local_gpu_rank": local_gpu_rank,
-                "gpu_info": [],
-            }
+                metric_dict = {
+                    "key": key,
+                    "timestamp": timestamp,
+                    "hostname": hostname,
+                    "cpu_util": cpu_util,
+                    "mem_util": mem_util,
+                    "ib_network_io": ib_network_io,
+                    "gpu_rank": gpu_rank,
+                    "local_gpu_rank": local_gpu_rank,
+                    "gpu_info": "none",
+                }
 
-            if os.getenv("CUDA_VISIBLE_DEVICES") is not None:
-                # Get the GPU info in this node
-                gpus = GPUtil.getGPUs()
+                if os.getenv("CUDA_VISIBLE_DEVICES") is not None and isinstance(local_gpu_rank, int):
+                    # Get the GPU device list in string format
+                    gpu_device_ids = os.getenv("CUDA_VISIBLE_DEVICES")
+                    gpu_device_id_list = gpu_device_ids.split(",")
 
-                gpu_device_ids = os.getenv("CUDA_VISIBLE_DEVICES")
-                gpu_device_id_list = gpu_device_ids.split(",")
+                    # Get the GPU device ID based on the local rank ID
+                    device_id = int(gpu_device_id_list[local_gpu_rank])
 
-                for device_id_str in gpu_device_id_list:
-                    try:
-                        device_id = int(device_id_str)
-                        selected_gpu = gpus[device_id]
+                    # Get the GPU info in this node
+                    gpus = GPUtil.getGPUs()
+                    selected_gpu = gpus[device_id]
 
-                        gpu_device_id = selected_gpu.id
-                        gpu_name = selected_gpu.name
-                        gpu_util = format_2_decimal(selected_gpu.load * 100)
-                        gpu_mem_util = format_2_decimal(selected_gpu.memoryUtil * 100)
+                    gpu_device_id = selected_gpu.id
+                    gpu_name = selected_gpu.name
+                    gpu_util = format_2_decimal(selected_gpu.load * 100)
+                    gpu_mem_util = format_2_decimal(selected_gpu.memoryUtil * 100)
 
-                        metric_dict["gpu_info"].append(
-                            {
-                                "gpu_device_id": gpu_device_id,
-                                "gpu_name": gpu_name,
-                                "gpu_util": gpu_util,
-                                "gpu_mem_util": gpu_mem_util,
-                            }
-                        )
-                    except ValueError:
-                        continue
+                    metric_dict["gpu_info"] = {
+                        "gpu_device_id": gpu_device_id,
+                        "gpu_name": gpu_name,
+                        "gpu_util": gpu_util,
+                        "gpu_mem_util": gpu_mem_util,
+                    }
 
-            es.put_metric_to_elastic_search(metric_dict)
+                es.put_metric_to_elastic_search(metric_dict)
+            except ValueError:
+                continue
 
             time.sleep(self.interval)
 

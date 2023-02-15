ed616d0f9862e8ab3f39176a087eb4b90c5511ea
黄婷 <huangting3@CN0014010744M.local>
2023-02-15 12:00:23 +0800

    add file handler and update example

--- colossalai/logging/es_logger.py
+++ colossalai/logging/es_logger.py
@@ -1,26 +1,40 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 from colossalai.logging.cmreslogging.handlers import CMRESHandler
 
 # Default elastic search server host
 DEFAULT_ES_HOST = "http://10.140.0.75:9200"
+# Default log file name
+DEFAULT_FILE_NAME = "python_es.log"
 
 
-def setup_logger(logger_name: str) -> logging.Logger:
+def setup_logger(logger_name: str, log_level=logging.INFO, file_name: str = DEFAULT_FILE_NAME) -> logging.Logger:
     """Configure the logger that is used for uniscale framework.
 
     Args:
         logger_name (str): Used to create or get the correspoding logger in
             getLogger call. It will get the root logger by default.
+        log_level : Default is logging.DEBUG.
+        file_name (str): Log file name, default is "python_es.log".
 
     Returns:
         logger (logging.Logger): the created or modified logger.
 
     """
 
-    handler = CMRESHandler(hosts=[DEFAULT_ES_HOST])
+    log_format = logging.Formatter("[%(levelname)s] %(asctime)s - %(message)s")
+
     logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
+
+    es_handler = CMRESHandler(hosts=[DEFAULT_ES_HOST])
+    es_handler.setFormatter(log_format)
+    es_handler.setLevel(logging.DEBUG)
+    logger.addHandler(es_handler)
+
+    file_handler = RotatingFileHandler(filename=file_name)
+    file_handler.setFormatter(log_format)
+    file_handler.setLevel(log_level)
+    logger.addHandler(file_handler)
 
     return logger
--- colossalai/logging/es_logger_example.py
+++ colossalai/logging/es_logger_example.py
@@ -6,8 +6,8 @@ logger = setup_logger(__file__)
 
 if __name__ == "__main__":
     logger.info("info for es-logging")
+    logger.warning("warning for es-logging")
     logger.debug("debug for es-logging")
     logger.error("error for es-logging")
-    logger.info("test for es-logging")
 
     time.sleep(1)
